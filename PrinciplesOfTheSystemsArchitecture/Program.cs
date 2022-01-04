957ebb2e1616c89345a9ffaabafd0fefbb0f047e
shimnamisan <mikan.sup.all@gmail.com>
2022-01-05 08:08:29 +0900

    [update] Quantityクラスを検証

--- PrinciplesOfTheSystemsArchitecture/Program.cs
+++ PrinciplesOfTheSystemsArchitecture/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 // 正しい数量を扱うための独自クラス（Quantityクラス）を定義する
+// 数量を１以上え100以下であるように制限したクラス
 class Quantity
 {
     static readonly int MIN = 1;
@@ -7,8 +8,9 @@ class Quantity
 
     int value;
 
-    Quantity(int value)
+    public Quantity(int value)
     {
+        // 初期化時に数値をチェック
         if(value < MIN) throw new Exception($"不正な値です： {MIN} 未満");
         if(value > MAX) throw new Exception($"不正な値です： {MAX} 超");
         this.value = value;
@@ -34,3 +36,19 @@ class Quantity
 }
 
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Quantity quantity = new Quantity(0); // System.Exception: '不正な値です： 1 未満'
+        //Quantity quantity = new Quantity(101); // System.Exception: '不正な値です： 100 超'
+
+        Quantity value_100 = new Quantity(100);
+        Quantity value_20 = new Quantity(20);
+        Quantity value_10 = new Quantity(10);
+
+        Console.WriteLine(value_100.CanAdd(value_10));
+        Console.WriteLine(value_20.CanAdd(value_10));
+
+    }
+}
